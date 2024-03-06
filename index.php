295e31b8a38c27e6bae970221df41902744bd640
Habibcoding <136930881+Habibcoding@users.noreply.github.com>
2024-03-06 18:57:41 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,249 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="style/fontawesome/css/all.min.css">
+  <title>My Website</title>
+</head>
+
+<body>
+  <!-- Header -->
+  <section id="header">
+    <div class="header container">
+      <div class="nav-bar">
+        <div class="brand">
+          <a href="#hero">
+            <h1><span>H</span>A<span>B</span>IB</h1>
+          </a>
+        </div>
+        <div class="nav-list">
+          <div class="hamburger">
+            <div class="bar"></div>
+          </div>
+          <ul>
+            <li><a href="#hero" data-after="Home">Home</a></li>
+            <li><a href="#services" data-after="Service">Services</a></li>
+            <li><a href="#projects" data-after="Projects">Projects</a></li>
+            <li><a href="#about" data-after="About">About</a></li>
+            <li><a href="#contact" data-after="Contact">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Header -->
+
+
+  <!-- Hero Section  -->
+  <section id="hero">
+    <div class="hero container">
+      <div>
+        <h1>Salut, <span></span></h1>
+        <h1>Mon nom c'est <span></span></h1>
+        <h1>HABIB <span></span></h1>
+        <a href="#projects" type="button" class="cta">Portfolio</a>
+      </div>
+    </div>
+  </section>
+  <!-- End Hero Section  -->
+
+  <!-- Service Section -->
+  <section id="services">
+    <div class="services container">
+      <div class="service-top">
+        <h1 class="section-title">Serv<span>i</span>ces</h1>
+        <p> Tout type de service disposnible</p>
+      </div>
+      <div class="service-bottom">
+        <div class="service-item">
+          <div class="icon"><img src="img/ordi.png" /></div>
+          <h2>Creation de site web...</h2>
+          <p>Disponible</p>
+        </div>
+        <div class="service-item">
+          <div class="icon"><img src="img/ordi.png" /></div>
+          <h2>Activation de windows et office..</h2>
+          <p> Disponible </p>
+        </div>
+        <div class="service-item">
+          <div class="icon"><img src="img/ordi.png" /></div>
+          <h2>Traitement de Text , Affiche , logo...</h2>
+          <p>Disponible</p>
+        </div>
+        <div class="service-item">
+          <div class="icon"><img src="img/ordi.png" /></div>
+          <h2>Installation de windows , office... </h2>
+          <p>Disponible</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Service Section -->
+
+  <!-- Projects Section -->
+  <section id="projects">
+    <div class="projects container">
+      <div class="projects-header">
+        <h1 class="section-title">Recent <span>Projects</span></h1>
+      </div>
+      <div class="all-projects">
+        <div class="project-item">
+          <div class="project-info">
+            <h1>Project 1</h1>
+            <h2>Coding is Love</h2>
+            <p>C'est la mise en place d'un site de pagination</p>
+          </div>
+          <div class="project-img">
+            <img src="./img/img-1.png" alt="img">
+          </div>
+        </div>
+        <div class="project-item">
+          <div class="project-info">
+            <h1>Project 2</h1>
+            <h2>Coding is Love</h2>
+            <p>site pour les eleves (en cour ).. </p>
+          </div>
+          <div class="project-img">
+            <img src="./img/img-1.png" alt="img">
+          </div>
+        </div>
+        <div class="project-item">
+          <div class="project-info">
+            <h1>Project 3</h1>
+            <h2>Coding is Love</h2>
+            <p>Site de vente en ligne (en cour )..</p>
+          </div>
+          <div class="project-img">
+            <img src="./img/img-1.png" alt="img">
+          </div>
+        </div>
+        <div class="project-item">
+          <div class="project-info">
+            <h1>Project 4</h1>
+            <h2>Coding is Love</h2>
+            <p>Photocopie en ligne c'est a dire vous avez expressement besoin de faire la photocopie de votre fiche ,
+              n'ésiter plus (en cour )..
+            </p>
+          </div>
+          <div class="project-img">
+            <img src="./img/img-1.png" alt="img">
+          </div>
+        </div>
+        <div class="project-item">
+          <div class="project-info">
+            <h1>Project 5</h1>
+            <h2>Coding is Love</h2>
+            <p>(en cour )..</p>
+          </div>
+          <div class="project-img">
+            <img src="./img/img-1.png" alt="img">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Projects Section -->
+
+  <!-- About Section -->
+  <section id="about">
+    <div class="about container">
+      <div class="col-left">
+        <div class="about-img">
+          <img src="./img/habib2.PNG" alt="img">
+        </div>
+      </div>
+      <div class="col-right">
+        <h1 class="section-title">A propos de  <span>moi</span></h1>
+        <h2>Front End Developer</h2>
+        <p>je suis en effet en informatique developpeur d'application , agée de 19 ans a l'institut supérieur louis le grand 
+          de Bouake
+        </p>
+        <a href="#" class="cta">Download Resume</a>
+      </div>
+    </div>
+  </section>
+  <!-- End About Section -->
+
+  <!-- Contact Section -->
+  <section id="contact">
+    <div class="contact container">
+      <div>
+        <h1 class="section-title">Contact <span>info</span></h1>
+      </div>
+      <div class="contact-items">
+        <div class="contact-item">
+          <div class="icon"><img src="img/contact.png" /></div>
+          <div class="contact-info">
+            <h1>Phone</h1>
+            <h2>+255 0150457183</h2>
+            <h2>+255 0585903637</h2>
+          </div>
+        </div>
+        <div class="contact-item">
+          <div class="icon">
+            <img src="img/Email2.png" />
+            <!-- <i class="fa fa-envelope" style="height:100%;width:100%;"></i> -->
+          </div>
+          <div class="contact-info">
+            <h1>Email</h1>
+            <h2>y.prog.dev@gmail.com</h2>
+            <h2>habibtoure2022@gmail.com</h2>
+          </div>
+        </div>
+        <div class="contact-item">
+          <div class="icon">
+            <img src="img/localisation.png" />
+          <!-- <i class="fa fa-location"></i> -->
+          </div>
+          <div class="contact-info">
+            <h1>Address</h1>
+            <h2>Bouake,Côte d'jvoire</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Contact Section -->
+
+  <!-- Footer -->
+  <section id="footer">
+    <div class="footer container">
+      <div class="brand">
+        <h1><span>H</span>A<span>B</span>IB</h1>
+      </div>
+      <h2>Your Complete Web Solution</h2>
+      <div class="social-icon">
+        <div class="social-item">
+          <a href="#">
+            <img src="img/face.png" />
+          </a>
+        </div>
+        <div class="social-item">
+          <a href="http://wa.me/qr/DDIIH32Y2QCKG1">
+          <img src="img/whatshap.png" />
+          </a>
+        </div>
+        <div class="social-item">
+          <a href="#">
+          <img src="img/youtube.png" />
+
+          </a>
+        </div>
+        <div class="social-item">
+          <a href="#">
+          <img src="img/telegram.png" />
+
+          </a>
+        </div>
+      </div>
+      <p>Copyright © 2024 Wajahat. All rights reserved</p>
+    </div>
+  </section>
+  <!-- End Footer -->
+  <script src="./app.js"></script>
+</body>
+
+</html>
